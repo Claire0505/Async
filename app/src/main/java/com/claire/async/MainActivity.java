286e5b4190bd8a4121d6787b94f917494d80ab48
Claire <claire0505@gmail.com>
2018-10-07 10:28:09 +0800

    使用Chain建立三個水平分佈的Button元件

--- app/src/main/java/com/claire/async/MainActivity.java
+++ app/src/main/java/com/claire/async/MainActivity.java
@@ -2,6 +2,7 @@ package com.claire.async;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void go1(View v){
+
+    }
+
+    public void go2(View v){
+
+    }
+
+    public void go3(View v){
+
     }
 }
